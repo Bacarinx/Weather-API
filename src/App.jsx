b0857cc454f03abd58423e0dfa26ef8a530e3f82
Henrique Bacarin Litoldo <113396099+Bacarinx@users.noreply.github.com>
2024-02-01 20:05:49 -0300

    html concluído

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const privateKey = import.meta.env.VITE_PRIVATE_KEY
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="form">
+        <h3>Previsão do clima para hoje da cidade:</h3>
+        <div className="form-input-container">
+          <input type="text" placeholder="Digite o nome da cidade" />
+          <button>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div id="weather-data">
+        <h2>
+          <i className="fa-solid fa-location-dot"></i>
+          <span id="city">Maceió</span>
+          <img src="" alt="Country" />
+        </h2>
+        <p id="temperature">
+          <span>38</span>&deg;C
         </p>
+        <div id="description-container">
+          <p id="description">Nublado</p>
+          <img src="" alt="Condição do tempo" id="weather-icon" />
+        </div>
+        <div id="Details-container">
+          <p id="umidity">
+            <i className="fa-solid fa-droplet"></i>
+            <span>48%</span>
+          </p>
+          <p id="wind">
+            <i className="fa-solid fa-wind"></i>
+            <span>3km/h</span>
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
