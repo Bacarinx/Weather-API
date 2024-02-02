3abae7792be7ce902c27494a64cb3643e7cc70da
Henrique Bacarin Litoldo <113396099+Bacarinx@users.noreply.github.com>
2024-02-02 11:51:44 -0300

    add background image to differents cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
 function App() {
   const privateKey = import.meta.env.VITE_PRIVATE_KEY
   const [city, setCity] = useState('')
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState()
+  // const body = document.querySelector('body')
+
+  async function dataSet(cityName) {
+    const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${privateKey}&lang=pt_br`
+    const response = await axios.get(apiWeather)
+    setData(response.data)
+    document.body.style.background = `url('https://source.unsplash.com/1600x1000/?Cidade-${cityName}')`
+    console.log(response.data)
+  }
 
   return (
     <div className="container">
@@ -25,39 +34,111 @@ function App() {
               const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${privateKey}&lang=pt_br`
               const response = await axios.get(apiWeather)
               setData(response.data)
+              console.log(response.data)
+              document.body.style.background = `url('https://source.unsplash.com/1600x1000/?Cidade-${city}')`
             }}
           >
             <i className="fa-solid fa-magnifying-glass"></i>
           </button>
         </div>
       </div>
-      <div id="weather-data">
-        <h2>
-          <i className="fa-solid fa-location-dot"></i>
-          <span id="city">{city}</span>
-          <img
-            src={`https://flagsapi.com/${data.sys.country}/flat/32.png`}
-            alt="Country"
-          />
-        </h2>
-        <p id="temperature">
-          <span>{data.main.temp.toFixed(1)}</span>&deg;C
-        </p>
-        <div id="description-container">
-          <p id="description">Nublado</p>
-          <img src="" alt="Condição do tempo" id="weather-icon" />
+
+      {!data ? (
+        <div className="buttons">
+          <button
+            value="Sorocaba"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Sorocaba
+          </button>
+          <button
+            value="Seul"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Seul
+          </button>
+          <button
+            value="Tokio"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Tókio
+          </button>
+          <button
+            value="Cancun"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Cancún
+          </button>
+          <button
+            value="New York"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Nova Yorque
+          </button>
+          <button
+            value="Jacarta"
+            className="btn"
+            onClick={(e) => {
+              setCity(e.target.value)
+              dataSet(e.target.value)
+            }}
+          >
+            Jacarta
+          </button>
         </div>
-        <div id="details-container">
-          <p id="umidity">
-            <i className="fa-solid fa-droplet"></i>
-            <span>48%</span>
+      ) : (
+        <div id="weather-data">
+          <h2>
+            <i className="fa-solid fa-location-dot"></i>
+            <span id="city">{data.name}</span>
+            <img
+              src={`https://flagsapi.com/${data.sys.country}/flat/32.png`}
+              alt="Country"
+            />
+          </h2>
+          <p id="temperature">
+            <span>{data.main.temp.toFixed(1)}</span>&deg;C
           </p>
-          <p id="wind">
-            <i className="fa-solid fa-wind"></i>
-            <span>3km/h</span>
-          </p>
+          <div id="description-container">
+            <p id="description">{data.weather[0].description}</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+              alt="Condição do tempo"
+              id="weather-icon"
+            />
+          </div>
+          <div id="details-container">
+            <p id="umidity">
+              <i className="fa-solid fa-droplet"></i>
+              <span>{data.main.humidity}%</span>
+            </p>
+            <p id="wind">
+              <i className="fa-solid fa-wind"></i>
+              <span>{data.wind.speed.toFixed(1)}km/h</span>
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
