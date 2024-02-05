9f6941a00eb54c583902332dd0311985ed879253
Henrique Bacarin Litoldo <113396099+Bacarinx@users.noreply.github.com>
2024-02-05 17:46:04 -0300

    Finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 
 function App() {
   const privateKey = import.meta.env.VITE_PRIVATE_KEY
   const [city, setCity] = useState('')
-  const [data, setData] = useState()
-  // const body = document.querySelector('body')
+  const [prevData, setPrevData] = useState(null)
 
-  async function dataSet(cityName) {
-    const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${privateKey}&lang=pt_br`
-    const response = await axios.get(apiWeather)
-    setData(response.data)
-    document.body.style.background = `url('https://source.unsplash.com/1600x1000/?Cidade-${cityName}')`
+  const { isLoading, error, refetch } = useQuery(
+    ['weather', city],
+    async () => {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${privateKey}&lang=pt_br`,
+      )
+      return response.data
+    },
+    {
+      enabled: false, // Don't fetch data automatically, wait for the user to trigger it
+      onSuccess: (fetchData) => {
+        setPrevData(fetchData)
+        document.body.style.background = `url('https://source.unsplash.com/1800x1200/?Cidade-${city}')`
+      },
+      staleTime: 1000,
+    },
+  )
+
+  const handleSetCity = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handlecallAPI = (e) => {
+    e.preventDefault()
+    refetch()
+  }
+
+  const handleButtonClick = async (e) => {
+    e.preventDefault()
+    await setCity(e.target.value)
+    refetch()
   }
 
   return (
@@ -20,108 +46,98 @@ function App() {
       <div className="form">
         <h3>Previsão do clima para hoje da cidade:</h3>
         <div className="form-input-container">
-          <input
-            type="text"
-            placeholder="Digite o nome da cidade"
-            onChange={(e) => {
-              setCity(e.target.value)
-            }}
-          />
-          <button
-            onClick={async (e) => {
-              e.preventDefault()
-              const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${privateKey}&lang=pt_br`
-              const response = await axios.get(apiWeather)
-              setData(response.data)
-              console.log(response.data)
-              document.body.style.background = `url('https://source.unsplash.com/1600x1000/?Cidade-${city}')`
-            }}
-          >
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
+          <form action="Post">
+            <input
+              type="text"
+              placeholder="Digite o nome da cidade"
+              onChange={handleSetCity}
+            />
+            <button
+              type="submit"
+              onClick={async (e) => {
+                handlecallAPI(e)
+              }}
+            >
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
         </div>
       </div>
 
-      {!data ? (
+      {isLoading ? (
+        <div className="carregando">
+          <strong>Carregando dados</strong>
+          <p>Aguarde um momento</p>
+        </div>
+      ) : error ? (
+        <div className="erros">
+          <p>
+            <strong>ERRO: </strong>Cidade não encontrada
+          </p>
+          <p>Pesquise o nome da cidade em inglês.</p>
+        </div>
+      ) : !prevData ? (
         <div className="buttons">
           <button
             value="Sorocaba"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
             Sorocaba
           </button>
           <button
             value="Seul"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
             Seul
           </button>
           <button
             value="Tokio"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
             Tókio
           </button>
           <button
             value="Cancun"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
             Cancún
           </button>
           <button
             value="New York"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
             Nova Yorque
           </button>
           <button
-            value="Jacarta"
+            value="Madrid"
             className="btn"
-            onClick={(e) => {
-              setCity(e.target.value)
-              dataSet(e.target.value)
-            }}
+            onClick={(e) => handleButtonClick(e)}
           >
-            Jacarta
+            Madrid
           </button>
         </div>
       ) : (
         <div id="weather-data">
           <h2>
             <i className="fa-solid fa-location-dot"></i>
-            <span id="city">{data.name}</span>
+            <span id="city">{prevData.name}</span>
             <img
-              src={`https://flagsapi.com/${data.sys.country}/flat/32.png`}
+              src={`https://flagsapi.com/${prevData.sys.country}/flat/32.png`}
               alt="Country"
             />
           </h2>
           <p id="temperature">
-            <span>{data.main.temp.toFixed(1)}</span>&deg;C
+            <span>{prevData.main.temp.toFixed(1)}</span>&deg;C
           </p>
           <div id="description-container">
-            <p id="description">{data.weather[0].description}</p>
+            <p id="description">{prevData.weather[0].description}</p>
             <img
-              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+              src={`https://openweathermap.org/img/wn/${prevData.weather[0].icon}.png`}
               alt="Condição do tempo"
               id="weather-icon"
             />
@@ -129,11 +145,11 @@ function App() {
           <div id="details-container">
             <p id="umidity">
               <i className="fa-solid fa-droplet"></i>
-              <span>{data.main.humidity}%</span>
+              <span>{prevData.main.humidity}%</span>
             </p>
             <p id="wind">
               <i className="fa-solid fa-wind"></i>
-              <span>{data.wind.speed.toFixed(1)}km/h</span>
+              <span>{prevData.wind.speed.toFixed(1)}km/h</span>
             </p>
           </div>
         </div>
